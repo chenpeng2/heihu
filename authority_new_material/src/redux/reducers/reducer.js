7b29836ed9faf5be679e6fe8403b7440d371581e
chenpeng <865707478@qq.com>
2020-03-19 11:46:36 +0800

    no message

--- authority_new_material/src/redux/reducers/reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import userInfo from './loginReducer'
-const rootReducer = combineReducers({
-    userInfo
-});
-
-export default rootReducer;
--- authority_new_material/src/redux/selectors/loginSelector.js
+++
@@ -1,1 +0,0 @@
-export const getloginState = store => store.userInfo;
